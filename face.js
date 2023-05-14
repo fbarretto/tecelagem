5b5e7f9542ecafc82a3748cb66858f2678e9e9bc
barretto <b4rretto@gmail.com>
2023-05-13 23:29:14 -0300

    Initial Commit

---
+++ face.js
@@ -0,0 +1,53 @@
+class Face {
+    constructor(x = width / 2, y = height / 2, w = 0, h = 0, easing = 0.5, timeout = 30) {
+        this.x = x;
+        this.y = y;
+        this.centerX = this.x + this.width / 2;
+        this.centerY = this.y + this.height / 2;
+        this.width = w;
+        this.height = h;
+        this.easing = easing;
+        this.color = [];
+        // this.color.push([0, 0, 0, 0]);
+        this.timeout = 300;
+    }
+
+    update(x, y, width, height) {
+        this.x += (x - this.x) * this.easing;
+        this.y += (y - this.y) * this.easing;
+        this.centerX = this.x + this.width / 2;
+        this.centerY = this.y + this.height / 2;
+        this.width += (width - this.width) * this.easing;
+        this.height += (height - this.height) * this.easing;
+        this.timeout = 300;
+    }
+
+    updateTimeout() {
+        this.timeout--;
+        return this.timeout < 0;
+    }
+
+    updateColor(color) {
+        this.color.push(color);
+    }
+
+    gotEmoji(x, y) {
+        let dist = Math.sqrt(Math.pow(this.centerX - x, 2) + Math.pow(this.centerY - y, 2));
+        if (dist < this.width * 0.3) {
+            return true;
+        }
+        return false;
+    }
+
+    render() {
+
+        let grad = drawingContext.createRadialGradient(this.centerX, this.centerY, 1, this.centerX, this.centerY, this.color.length * this.width * 0.2);
+        
+        if (this.color.length > 1)
+            for (let i = 0; i < this.color.length; i++) {
+                grad.addColorStop(map(i, 0, this.color.length - 1, 0, 1), color(this.color[i][0], this.color[i][1], this.color[i][2], this.timeout*170/300 ));
+            }
+        drawingContext.fillStyle = grad;
+        ellipse(this.centerX, this.centerY, this.color.length * this.width * 0.2, this.color.length * this.width * 0.2);
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,94 @@
+let faceapi;
+let video;
+let faces = [];
+
+let x, y;
+let emojiIndex = 0;
+let emojis = ["☘️", "🌸", "🍓", "🍺", "🐳", "💩", "💀"];
+let colors = [[135, 242, 77, 170], [255, 182, 231, 170], [165, 35, 40, 170], [255, 188, 45, 170], [52, 168, 201, 170], [142, 91, 54, 170], [229, 232, 232, 170]];
+let empty = 30;
+
+const detection_options = {
+    withLandmarks: true,
+    withDescriptors: false,
+}
+
+function setup() {
+    createCanvas(800, 600);
+    video = createCapture(VIDEO);
+    video.size(width, height);
+    video.hide();
+    faceapi = ml5.faceApi(video, detection_options, modelReady);
+
+    noStroke();
+    textAlign(CENTER, CENTER);
+    randomEmojiPosition();
+}
+
+function randomEmojiPosition() {
+    x = random(width*0.1, width*0.9);
+    y = random(height*0.1, height*0.9);
+}
+
+function modelReady() {
+    console.log('ready!')
+    console.log(faceapi)
+    faceapi.detect(gotResults)
+
+}
+
+function gotResults(err, result) {
+    if (err) {
+        console.log(err)
+        return
+    }
+
+    if (result) {
+        // console.log(result);
+        if (result.length > 0) {
+            if (result.length <= faces.length) {
+                faces.length = result.length;
+            }
+            for (let i = faces.length; i < result.length; i++) {
+                faces.push(new Face());
+            }
+            for (let i = 0; i < result.length; i++) {
+                let x = result[i].alignedRect._box._x;
+                let y = result[i].alignedRect._box._y;
+                let w = result[i].alignedRect._box._width;
+                let h = result[i].alignedRect._box._height;
+                faces[i].update(x, y, w, h);
+            }
+            
+        }
+
+        for (let i = 0; i < faces.length; i++) {
+            if (faces[i].updateTimeout()) {
+                faces.splice(i, 1);
+            }
+        }
+    }
+    faceapi.detect(gotResults);
+}
+
+function draw() {
+    push();
+    translate(width, 0);
+    scale(-1, 1);
+    image(video, 0, 0, width, height);
+    for (face of faces) {
+        face.render();
+        if (face.gotEmoji(x, y)) {
+            randomEmojiPosition();
+            face.updateColor(colors[emojiIndex]);
+            emojiIndex = (emojiIndex + 1) % emojis.length;
+            // face.color = colors[emojiIndex];
+            
+        }
+    }
+
+    fill(255,255);
+    textSize(50);
+    text(emojis[emojiIndex], x, y);
+    pop();
+}
